27a2083f755148967b238bf7ab4e31c0d642e514
Jackson <43098221+SpikedJackson@users.noreply.github.com>
2025-01-30 14:05:54 -0500

    predicted on eval_data

--- poisson.py
+++ poisson.py
@@ -7,6 +7,7 @@ import statsmodels.api as sm
 
 # Load data
 data = pd.read_excel('data/modelling_data.xlsx')
+eval_data = pd.read_excel('data/evaluation_data.xlsx')
 
 
 # Define categorical and other columns
@@ -27,16 +28,21 @@ for col in categorical_columns:
     )
 
 # Train-test split
-initial_X_train, X_test, initial_y_train, y_test = train_test_split(
-    data.drop(target_column, axis=1),
-    data[target_column],
-    test_size=0.2,
-    random_state=42
-)
-df = pd.DataFrame(X_test, columns=data.columns)
-df['claimcount'] = y_test
+# initial_X_train, X_test, initial_y_train, y_test = train_test_split(
+#     data.drop(target_column, axis=1),
+#     data[target_column],
+#     test_size=0.2,
+#     random_state=42
+# )
 
+# Train on all of modelling_data and test on all of evalation_data
+initial_y_train = data[target_column]
+initial_X_train = data.drop(target_column, axis=1)
+y_test = eval_data[target_column]
+X_test = eval_data.drop(target_column, axis=1)
 
+# Prepare output
+df = pd.DataFrame(X_test, columns=['ROW_ID','exposure'])
 
 # Prepare training data
 X_train = initial_X_train.copy()
@@ -67,21 +73,16 @@ X_test_prepared = np.hstack((X_test_scaled_numerical, X_test_encoded_categorical
 X_train_prepared = sm.add_constant(X_train_prepared)
 X_test_prepared = sm.add_constant(X_test_prepared)
 
-
-
+# Build model using poisson family
 model = sm.GLM(y_train, X_train_prepared, family=sm.families.Poisson(), offset=X_train['log_exposure'].values)
 result = model.fit()
-    
 
-
-# Predict on the test set
-offset_test = X_test['log_exposure'].values  # Ensure correct offset shape
+# Use model to predict on the test set
+offset_test = X_test['log_exposure'].values
 y_pred = result.predict(X_test_prepared, offset=offset_test)
 df['prediction'] = y_pred
 df.to_excel('data/predictions.xlsx')
 
-
 # Calculate RMSE
-rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-print(f'RMSE on Test Data: {rmse}')
-
+# rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+# print(f'RMSE on Test Data: {rmse}')
