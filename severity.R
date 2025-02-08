f756cc345711ffc2eac04dbc610a2b616e33e385
Jackson <43098221+SpikedJackson@users.noreply.github.com>
2025-02-08 11:29:00 -0500

    severity model

---
+++ severity.R
@@ -0,0 +1,20 @@
+library(readxl)
+library(openxlsx)
+
+my_data <- read_excel("C:/Users/Jacks/Desktop/Co-operators-Case-Competition/data/joined_data.xlsx")
+
+model <- readRDS("C:/Users/Jacks/Desktop/Co-operators-Case-Competition/glm_severity_model", refhook = NULL)
+
+new_data <- my_data[, c("credit_band", "ownership", "loyalty", "claims_history")]
+
+summary(model)
+
+dummy.coef(model)
+
+prediction <- predict(model, data=my_data)
+
+prediction
+
+df = data.frame(matrix(prediction, ncol = 1, byrow = T ))
+
+write.xlsx(df, "C:/Users/Jacks/Desktop/Co-operators-Case-Competition/data/severity.xlsx")
